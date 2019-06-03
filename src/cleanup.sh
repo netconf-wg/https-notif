edb018ab79a368cee891ee485227b60bbd392251
Mahesh Jethanandani <mjethanandani@gmail.com>
2019-06-03 15:58:14 -0700

    Updates to Initial Version

---
+++ src/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh  
+
+rm ietf-https-notif\@*.yang 
+
---
+++ src/sed.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DATE=`date +%Y-%m-%d`
+
+sed_nc_yang_file() {
+   # $1 is the src directory
+   # $2 is the dest directory
+   # $3 is the YANG module
+   sed -e"s/YYYY-MM-DD/$DATE/" $1/$3.yang > $2/$3\@$DATE.yang
+}
+
+sed_nc_yang_file yang     ../bin      ietf-https-notif
