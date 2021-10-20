5a29d330f30dcce99c4a8731a1df11ecd59c457e
Mahesh Jethanandani <mjethanandani@gmail.com>
2021-10-20 16:46:07 -0700

    Getting draft ready for publication

--- src/validate-and-gen-trees.sh
+++ src/validate-and-gen-trees.sh
@@ -3,11 +3,11 @@
 for i in ../bin/*\@$(date +%Y-%m-%d).yang
 do
     name=$(echo $i | cut -f 1-3 -d '.')
-    echo "Validating YANG module $name.yang"
+    echo "Validating YANG module $name.yang using pyang"
     if test "${name#^example}" = "$name"; then
-        response=`pyang --lint --strict --canonical -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+        response=`pyang --lint --strict --canonical -p ../../iana/yang-parameters -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
     else            
-        response=`pyang --ietf --strict --canonical -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+        response=`pyang --ietf --strict --canonical -p ../../iana/yang-parameters -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
     fi
     if [ $? -ne 0 ]; then
         printf "$name.yang failed pyang validation\n"
@@ -16,7 +16,9 @@ do
 	rm yang/*-tree.txt.tmp
         exit 1
     fi
+    echo "Validation of YANG module $name.yang using pyang succeeded."
     fold -w 71 $name-tree.txt.tmp > $name-tree.txt
+    echo "Validating YANG module $name.yang using yanglint"
     response=`yanglint -p ../src/yang $name.yang -i`
     if [ $? -ne 0 ]; then
        printf "$name.yang failed yanglint validation\n"
@@ -24,7 +26,7 @@ do
        echo
        exit 1
    fi
-   echo "$name.yang is VALID"
+   echo "Validation of $name.yang using yanglint succeeded."
 done
 rm ../bin/*-tree.txt.tmp
 
@@ -33,9 +35,9 @@ do
     name=$(echo $i | cut -f 1-3 -d '.')
     echo "Generating abridged tree diagram for $name.yang"
     if test "${name#^example}" = "$name"; then
-       response=`pyang --lint --strict --canonical -p ../bin/submodules -p ../bin -f tree --tree-depth=7 --max-line-length=69 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
+       response=`pyang --lint --strict --canonical -p ../../iana/yang-parameters/ -p ../bin -f tree --tree-depth=7 --max-line-length=69 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
     else            
-       response=`pyang --ietf --strict --canonical -p ../bin/submodules -p ../bin -f tree --tree-depth=3 --max-line-length=69 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
+       response=`pyang --ietf --strict --canonical -p ../../iana/yang-parameters/ -p ../bin -f tree --tree-depth=3 --max-line-length=69 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
     fi
     if [ $? -ne 0 ]; then
         printf "$name.yang failed generation of sub-tree diagram\n"
@@ -54,7 +56,7 @@ for i in yang/example-https-notif-*.xml
 do
     name=$(echo $i | cut -f 1-2 -d '.')
     echo "Validating examples for $name.xml"
-    response=`yanglint -s -i -t auto -p ../bin ../bin/ietf-https-notif-transport\@$(date +%Y-%m-%d).yang $name.xml`
+    response=`yanglint -ii -t config -p ../bin -p ../../iana/yang-parameters ../bin/ietf-https-notif-transport\@$(date +%Y-%m-%d).yang $name.xml`
     if [ $? -ne 0 ]; then
        printf "failed (error code: $?)\n"
        printf "$response\n\n"
@@ -68,7 +70,7 @@ for i in yang/example-custom-https-notif.xml
 do
     name=$(echo $i | cut -f 1 -d '.')
     echo "Validating examples for $name.xml"
-    response=`yanglint -s -i -t auto -p ../bin ../bin/example-custom-module\@$(date +%Y-%m-%d).yang $name.xml`
+    response=`yanglint -ii -t config -p ../bin -p ../../iana/yang-parameters ../bin/example-custom-module\@$(date +%Y-%m-%d).yang $name.xml`
     if [ $? -ne 0 ]; then
        printf "failed (error code: $?)\n"
        printf "$response\n\n"
